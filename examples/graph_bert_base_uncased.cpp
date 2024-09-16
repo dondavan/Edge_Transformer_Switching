4649d5e78a7be15150ec8b47139a8797d2cb9820
dondavan <yeezushugh@gmail.com>
2024-09-16 15:55:53 +0200

    compile

--- examples/graph_bert_base_uncased.cpp
+++ examples/graph_bert_base_uncased.cpp
@@ -65,9 +65,16 @@ class GraphVanillaTransformerExample : public Example
         std::string data_path = common_params.data_path;
 
         // Model parameters
+        constexpr unsigned int d_model    = 768U;   // Dim layer output
+        constexpr unsigned int d_vocab    = 30522U; // Vocaboary size
+        constexpr unsigned int d_segemnt  = 2U;     // Sentence segmentation size
+        constexpr unsigned int d_position = 512U;   // Pretrained positional encoding length
 
         // Create input tensor
         const TensorShape src_tensor = TensorShape(common_params.input_len);
+
+        // Data layout
+        const DataLayout operation_layout = DataLayout::NCHW;
 
         TensorDescriptor input_descriptor = TensorDescriptor(src_tensor, common_params.data_type);
 
@@ -82,7 +89,17 @@ class GraphVanillaTransformerExample : public Example
                             get_segment_accessor(common_params.segment, move(at2_preproccessor)))
                      .set_name("in1")
 
-              << OutputLayer(get_output_accessor(common_params)).set_name("out1");
+              << EmbeddingLayer(EmbeddingLayerInfo(d_model,
+                                                   d_vocab,
+                                                   d_segemnt,
+                                                   d_position,
+                                                   true /*Use pretrained positional encoding*/,
+                                                   ConvertPolicy::SATURATE),
+                                get_weights_accessor(data_path, "token_embedding.npy", operation_layout),
+                                get_weights_accessor(data_path, "segment_embedding.npy", operation_layout),
+                                get_weights_accessor(data_path, "positional_embedding.npy", operation_layout))
+                     .set_name("tkemb1")
+<< OutputLayer(get_output_accessor(common_params)).set_name("out1");
 
         // Finalize graph
         GraphConfig config;
