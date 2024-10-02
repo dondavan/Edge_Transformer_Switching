5a8af3e9f9ff16f44593e362e7ab45c4d103539c
dondavan <yeezushugh@gmail.com>
2024-10-02 14:29:52 +0200

    verifying tensior

--- src/cpu/kernels/CpuPositionEmbeddingKernel.cpp
+++ src/cpu/kernels/CpuPositionEmbeddingKernel.cpp
@@ -43,7 +43,8 @@ void run_position_embedding(const Window &window, const ITensor *src, const ITen
     Iterator vector_iter(vector,win);
 
     const auto dst_ptr      = reinterpret_cast<float *>(dst_iter.ptr());
-    //const auto vector_ptr   = reinterpret_cast<float *>(vector_iter.ptr());
+    const auto vector_ptr   = reinterpret_cast<float *>(vector_iter.ptr());
+    ARM_COMPUTE_UNUSED(dst_ptr,vector_ptr);
 
     std::cout << "run_position_embedding 1" << std::endl;
 
@@ -56,8 +57,6 @@ void run_position_embedding(const Window &window, const ITensor *src, const ITen
             std::cout << "run_position_embedding 2" << std::endl;
                 offset_dst     = x * vector_depth;
                 offset_vector  = x * vector_depth;
-                //std::memcpy(dst_ptr + offset_dst, vector_ptr + offset_vector, (vector_depth) * sizeof(*vector_ptr));  
-                *dst_ptr = 1;
                 
     std::cout << "run_position_embedding 3" << std::endl;          
             }
