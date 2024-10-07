2b7664e662002737df2651486508605059a842d3
dondavan <yeezushugh@gmail.com>
2024-10-07 14:03:26 +0200

    Cleaned and running

--- src/runtime/NEON/functions/NELayerNormLayer.cpp
+++ src/runtime/NEON/functions/NELayerNormLayer.cpp
@@ -69,10 +69,36 @@ void NELayerNormLayer::run()
 
     ITensorPack pack;
 
+    std::cout << "switching/src/runtime/NEON/functions/NELayerNormLayer.cpp" <<std::endl;
+
+    std::cout <<  _impl->src->info()->tensor_shape().x() << std::endl;
+    std::cout <<  _impl->src->info()->tensor_shape().y() << std::endl;
+    std::cout <<  _impl->src->info()->tensor_shape().z() << std::endl;
+    std::cout << *reinterpret_cast<float *>( _impl->src->ptr_to_element(Coordinates(0,0,0))) <<std::endl;
+    std::cout << *reinterpret_cast<float *>( _impl->src->ptr_to_element(Coordinates(1,0,0))) <<std::endl;
+    std::cout << *reinterpret_cast<float *>( _impl->src->ptr_to_element(Coordinates(2,0,0))) <<std::endl;
+
+
+    std::cout << *reinterpret_cast<float *>( _impl->src->ptr_to_element(Coordinates(765,6,0))) <<std::endl;
+    std::cout << *reinterpret_cast<float *>( _impl->src->ptr_to_element(Coordinates(766,6,0))) <<std::endl;
+    std::cout << *reinterpret_cast<float *>( _impl->src->ptr_to_element(Coordinates(767,6,0))) <<std::endl;
+
     pack.add_tensor(TensorType::ACL_SRC, _impl->src);
     pack.add_tensor(TensorType::ACL_DST, _impl->dst);
     
     _impl->op->run(pack);
+
+    std::cout << _impl->dst->info()->tensor_shape().x() << std::endl;
+    std::cout << _impl->dst->info()->tensor_shape().y() << std::endl;
+    std::cout << _impl->dst->info()->tensor_shape().z() << std::endl;
+    std::cout << *reinterpret_cast<float *>(_impl->dst->ptr_to_element(Coordinates(0,0,0))) <<std::endl;
+    std::cout << *reinterpret_cast<float *>(_impl->dst->ptr_to_element(Coordinates(1,0,0))) <<std::endl;
+    std::cout << *reinterpret_cast<float *>(_impl->dst->ptr_to_element(Coordinates(2,0,0))) <<std::endl;
+
+
+    std::cout << *reinterpret_cast<float *>(_impl->dst->ptr_to_element(Coordinates(765,6,0))) <<std::endl;
+    std::cout << *reinterpret_cast<float *>(_impl->dst->ptr_to_element(Coordinates(766,6,0))) <<std::endl;
+    std::cout << *reinterpret_cast<float *>(_impl->dst->ptr_to_element(Coordinates(767,6,0))) <<std::endl;
 
 #ifdef MEASURE_TIME
     auto   end_time  = std::chrono::high_resolution_clock::now();
