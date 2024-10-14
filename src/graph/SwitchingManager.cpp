d865fa4a90a25ecdc183e69ac76d28c09f886e17
dondavan <yeezushugh@gmail.com>
2024-10-14 14:05:20 +0200

    target

--- src/graph/SwitchingManager.cpp
+++ src/graph/SwitchingManager.cpp
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-#include "arm_compute/graph/backends/SWITCH/SWDeviceBackend.h"
+#include "arm_compute/graph/SwitchingManager.h"
 
 #include "arm_compute/core/TensorInfo.h"
 #include "arm_compute/graph/backends/BackendRegistrar.h"
