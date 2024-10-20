3d5d18dcdaf725ca7799827b9f6fdf18f2814034
dondavan <yeezushugh@gmail.com>
2024-10-20 16:21:36 +0200

    added tensor target

--- src/gpu/cl/kernels/ClLinearKernel.cpp
+++ src/gpu/cl/kernels/ClLinearKernel.cpp
@@ -113,9 +113,6 @@ void ClLinearKernel::run_op(ITensorPack &tensors, const Window &window, cl::Comm
     bias->info()->tensor_target_type() == TensorTargetType::CL? std::cout << "CL tensor" << std::endl : std::cout << "NEON tensor" << std::endl;
     dst->info()->tensor_target_type() == TensorTargetType::CL? std::cout << "CL tensor" << std::endl : std::cout << "NEON tensor" << std::endl; 
 
-    
-    std::cout << *reinterpret_cast<float *>(lhs->ptr_to_element(Coordinates(0,0,0))) << std::endl;
-
     unsigned int idx              = 0;
     Window       window_collapsed = window.collapse(ICLKernel::window(), Window::DimZ);
 
