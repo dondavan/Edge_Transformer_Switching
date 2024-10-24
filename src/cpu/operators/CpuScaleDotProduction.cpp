a6006bdd24fc1e28056de7e723429c7305969f49
dondavan <yeezushugh@gmail.com>
2024-10-24 18:14:09 +0200

    switching

--- src/cpu/operators/CpuScaleDotProduction.cpp
+++ src/cpu/operators/CpuScaleDotProduction.cpp
@@ -175,27 +175,27 @@ void CpuScaleDotProduction::run(ITensorPack &tensors)
     {
         ITensor *query_nc = const_cast<ITensor *>(query);
         query_cl          = static_cast<ICLTensor *>(query_nc);
-        query_cl->map(CLScheduler::get().queue(),false);
+        query_cl->map(CLScheduler::get().queue());
     }
 
     if(key->info()->tensor_target_type() == TensorTargetType::CL)
     {
         ITensor *key_nc = const_cast<ITensor *>(key);
         key_cl          = static_cast<ICLTensor *>(key_nc);
-        key_cl->map(CLScheduler::get().queue(),false);
+        key_cl->map(CLScheduler::get().queue());
     }
 
     if(value->info()->tensor_target_type() == TensorTargetType::CL)
     {
         ITensor *value_nc = const_cast<ITensor *>(value);
         value_cl          = static_cast<ICLTensor *>(value_nc);
-        value_cl->map(CLScheduler::get().queue(),false);
+        value_cl->map(CLScheduler::get().queue());
     }
 
     if(output->info()->tensor_target_type() == TensorTargetType::CL)
     {
         output_cl          = static_cast<ICLTensor *>(output);
-        output_cl->map(CLScheduler::get().queue(),false);
+        output_cl->map(CLScheduler::get().queue());
     }
 
     std::cout<< "query: "<< *reinterpret_cast<float *>(query->ptr_to_element(Coordinates(0,0,0))) <<std::endl;
