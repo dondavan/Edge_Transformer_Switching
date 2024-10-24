20cb0fd85bf6cda40ed625f05480366dac695e86
dondavan <yeezushugh@gmail.com>
2024-10-24 20:48:40 +0200

    switching

--- src/cpu/operators/CpuScaleDotProduction.cpp
+++ src/cpu/operators/CpuScaleDotProduction.cpp
@@ -215,7 +215,7 @@ void CpuScaleDotProduction::run(ITensorPack &tensors)
         }
     }
     
-
+    *reinterpret_cast<float *>(query->ptr_to_element(Coordinates(0,0,0))) = _recurrence_count;
     std::cout<< "query: "<< *reinterpret_cast<float *>(query->ptr_to_element(Coordinates(0,0,0))) <<std::endl;
     std::cout<< "key: "<< *reinterpret_cast<float *>(key->ptr_to_element(Coordinates(0,0,0))) <<std::endl;
     std::cout<< "value: "<< *reinterpret_cast<float *>(value->ptr_to_element(Coordinates(0,0,0))) <<std::endl;
