856eafa200861c67f486a19859adbcadc371b330
dondavan <yeezushugh@gmail.com>
2024-10-24 21:00:35 +0200

    switching

--- src/runtime/CL/CLTensorAllocator.cpp
+++ src/runtime/CL/CLTensorAllocator.cpp
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-#include<stdio>
+#include<iostream>
 #include "arm_compute/runtime/CL/CLTensorAllocator.h"
 
 #include "arm_compute/core/Error.h"
