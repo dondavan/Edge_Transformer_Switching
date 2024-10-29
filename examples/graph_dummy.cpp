5c512e751269d1958984052e6924c44a5bf9de9d
dondavan <yeezushugh@gmail.com>
2024-10-29 13:48:07 +0100

    cleaning

---
+++ examples/graph_dummy.cpp
@@ -0,0 +1,153 @@
+/*
+ * Copyright (c) 2017-2021 Arm Limited.
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#include "arm_compute/graph.h"
+
+#include "support/ToolchainSupport.h"
+#include "utils/CommonGraphOptions.h"
+#include "utils/GraphUtils.h"
+#include "utils/Utils.h"
+
+using namespace arm_compute;
+using namespace arm_compute::utils;
+using namespace arm_compute::graph::frontend;
+using namespace arm_compute::graph_utils;
+
+/** Example demonstrating how to implement MobileNet's network using the Compute Library's graph API */
+class GraphMobilenetExample : public Example
+{
+    public:
+    GraphMobilenetExample()
+        : cmd_parser(), common_opts(cmd_parser), common_params(), graph(0, "MobileNetV1")
+    {
+        // Add model id option
+        model_id_opt = cmd_parser.add_option<SimpleOption<int>>("model-id", 0);
+        model_id_opt->set_help("Mobilenet model id (0: 1.0_224, else: 0.75_160");
+    }
+    GraphMobilenetExample(const GraphMobilenetExample &)            = delete;
+    GraphMobilenetExample &operator=(const GraphMobilenetExample &) = delete;
+    ~GraphMobilenetExample() override                               = default;
+    bool do_setup(int argc, char **argv) override
+    {
+        // Parse arguments
+        cmd_parser.parse(argc, argv);
+        cmd_parser.validate();
+
+        // Consume common parameters
+        common_params = consume_common_graph_parameters(common_opts);
+
+        // Return when help menu is requested
+        if(common_params.help)
+        {
+            cmd_parser.print_help(argv[0]);
+            return false;
+        }
+
+        // Print parameter values
+        std::cout << common_params << std::endl;
+
+        // Get model parameters
+        int model_id = model_id_opt->value();
+
+        // Create input descriptor
+        unsigned int spatial_size = (model_id == 0 || common_params.data_type == DataType::QASYMM8) ? 224 : 160;
+
+        // Create input descriptor
+        const TensorShape tensor_shape =
+            permute_shape(TensorShape(spatial_size, spatial_size, 3U, common_params.batches), DataLayout::NCHW,
+                          common_params.data_layout);
+        TensorDescriptor input_descriptor =
+            TensorDescriptor(tensor_shape, common_params.data_type).set_layout(common_params.data_layout);
+
+        // Set graph hints
+        graph << common_params.target << common_params.fast_math_hint;
+        float       depth_scale = (model_id == 0) ? 1.f : 0.75;
+        std::unique_ptr<IPreprocessor> preprocessor = std::make_unique<TFPreproccessor>();
+
+        graph << InputLayer(input_descriptor, get_input_accessor(common_params, std::move(preprocessor), false))
+              << ConvolutionLayer(3U, 3U, 32U * depth_scale,
+                                  get_weights_accessor("", "Conv2d_0_weights.npy", DataLayout::NCHW),
+                                  std::unique_ptr<arm_compute::graph::ITensorAccessor>(nullptr),
+                                  PadStrideInfo(2, 2, 0, 1, 0, 1, DimensionRoundingType::FLOOR))
+                     .set_name("Conv2d_0")
+              << BatchNormalizationLayer(get_weights_accessor("", "Conv2d_0_BatchNorm_moving_mean.npy"),
+                                         get_weights_accessor("", "Conv2d_0_BatchNorm_moving_variance.npy"),
+                                         get_weights_accessor("", "Conv2d_0_BatchNorm_gamma.npy"),
+                                         get_weights_accessor("", "Conv2d_0_BatchNorm_beta.npy"), 0.001f)
+                     .set_name("Conv2d_0/BatchNorm")
+              << ActivationLayer(ActivationLayerInfo(ActivationLayerInfo::ActivationFunction::BOUNDED_RELU, 6.f))
+                     .set_name("Conv2d_0/Relu6")
+              << ConvolutionLayer(3U, 3U, 32U * depth_scale,
+                                  get_weights_accessor("", "Conv2d_0_weights.npy", DataLayout::NCHW),
+                                  std::unique_ptr<arm_compute::graph::ITensorAccessor>(nullptr),
+                                  PadStrideInfo(2, 2, 0, 1, 0, 1, DimensionRoundingType::FLOOR))
+                     .set_name("Conv2d_0");
+
+                 // Create common tail
+        graph << OutputLayer(get_output_accessor(common_params, 5));
+
+        // Finalize graph
+        GraphConfig config;
+        config.num_threads = common_params.threads;
+        config.use_tuner   = common_params.enable_tuner;
+        config.tuner_mode  = common_params.tuner_mode;
+        config.tuner_file  = common_params.tuner_file;
+        config.mlgo_file   = common_params.mlgo_file;
+
+        graph.finalize(common_params.target, config);
+
+        return true;
+    }
+    void do_run() override
+    {
+        // Run graph
+        graph.run();
+    }
+
+    private:
+    CommandLineParser  cmd_parser;
+    CommonGraphOptions common_opts;
+    SimpleOption<int> *model_id_opt{ nullptr };
+    CommonGraphParams  common_params;
+    Stream             graph;
+};
+
+/** Main program for MobileNetV1
+ *
+ * Model is based on:
+ *      https://arxiv.org/abs/1704.04861
+ *      "MobileNets: Efficient Convolutional Neural Networks for Mobile Vision Applications"
+ *      Andrew G. Howard, Menglong Zhu, Bo Chen, Dmitry Kalenichenko, Weijun Wang, Tobias Weyand, Marco Andreetto, Hartwig Adam
+ *
+ * Provenance: download.tensorflow.org/models/mobilenet_v1_2018_08_02/mobilenet_v1_1.0_224.tgz
+ *             download.tensorflow.org/models/mobilenet_v1_2018_08_02/mobilenet_v1_0.75_160.tgz
+ *
+ * @note To list all the possible arguments execute the binary appended with the --help option
+ *
+ * @param[in] argc Number of arguments
+ * @param[in] argv Arguments
+ */
+int main(int argc, char **argv)
+{
+    return arm_compute::utils::run_example<GraphMobilenetExample>(argc, argv);
+}
