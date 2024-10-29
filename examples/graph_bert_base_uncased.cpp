7cb8fa0d10a1fa9a231cc395c4b643c88759193b
dondavan <yeezushugh@gmail.com>
2024-10-29 14:31:41 +0100

    cleaning

--- examples/graph_bert_base_uncased.cpp
+++ examples/graph_bert_base_uncased.cpp
@@ -71,7 +71,7 @@ class GraphVanillaTransformerExample : public Example
         constexpr unsigned int d_position = 512U;   // Pretrained positional encoding length
         constexpr unsigned int h          = 12U;    // Parallel attention (Heads)
         constexpr float        eps        = 1e-12;  // Layer normalization eplision
-        constexpr unsigned int d_ff       = 3072U;  // Dim feedforward
+        constexpr unsigned int d_ff       = 268U;  // Dim feedforward
 
         // Create input tensor
         const TensorShape src_tensor = TensorShape(common_params.input_len);
