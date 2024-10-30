e288dac99b84d02ae603707549af202ddf6dbc1f
dondavan <yeezushugh@gmail.com>
2024-10-30 11:11:46 +0100

    cleaning

--- arm_compute/graph/backends/FunctionHelpers.h
+++ arm_compute/graph/backends/FunctionHelpers.h
@@ -56,7 +56,7 @@ class CPUWrapperFunction : public IFunction
     public:
     /* Default constructor */
     CPUWrapperFunction()
-        : _tensor_handles(), _tensors(), _func(nullptr),
+        : _tensor_handles(), _tensors(), _func(nullptr)
     {
     }
 
