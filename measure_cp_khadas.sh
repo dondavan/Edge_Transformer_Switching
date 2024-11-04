074cfbfc378022387a193b452959d457e9034a26
dondavan <yeezushugh@gmail.com>
2024-11-04 13:23:29 +0100

    distillk

--- measure_cp_khadas.sh
+++ measure_cp_khadas.sh
@@ -2,4 +2,5 @@ git pull
 scons Werror=1 debug=0 asserts=0 neon=1 opencl=1 examples=1 os=linux arch=armv8a measure=1 -j4
 sshpass -p 'khadas' scp build/libarm_compute* Khadas.local:/home/khadas/run/lib 
 sshpass -p 'khadas' scp build/examples/graph_bert_* Khadas.local:/home/khadas/run
+sshpass -p 'khadas' scp build/examples/graph_distill_bert* Khadas.local:/home/khadas/run
 sshpass -p 'khadas' scp build/examples/graph_dummy* Khadas.local:/home/khadas/run
