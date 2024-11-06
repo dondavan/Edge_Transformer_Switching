428d81165f1797e92eb9b3bc467ff965b34aab14
dondavan <yeezushugh@gmail.com>
2024-11-06 13:30:37 +0100

    attention_conv

--- examples/graph_bert_base_uncased.cpp
+++ examples/graph_bert_base_uncased.cpp
@@ -154,10 +154,7 @@ class GraphVanillaTransformerExample : public Example
     {
         auto start_time = std::chrono::high_resolution_clock::now();
 
-        for(int i = 0; i <4 ;i ++)
-        {
-            graph.run();   
-        }
+        graph.run();
 
         auto   end_time  = std::chrono::high_resolution_clock::now();
         double cost_time = std::chrono::duration_cast<std::chrono::duration<double>>(end_time - start_time).count();
